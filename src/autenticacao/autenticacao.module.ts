5d10646273cfddcd9779660b7ba2abacfe201f50
ramonfrombr <ramonfrombr>
2023-10-29 13:26:48 -0300

    09-autenticacao-jwt

--- src/autenticacao/autenticacao.module.ts
+++ src/autenticacao/autenticacao.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AutenticacaoController } from './autenticacao.controller';
 import { AutenticacaoService } from './autenticacao.service';
-import { PrismaModule } from 'src/prisma/prisma.module';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './strategy';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [JwtModule.register({})],
   controllers: [AutenticacaoController],
-  providers: [AutenticacaoService],
+  providers: [AutenticacaoService, JwtStrategy],
 })
 export class AutenticacaoModule {}
--- src/autenticacao/autenticacao.service.ts
+++ src/autenticacao/autenticacao.service.ts
@@ -3,10 +3,43 @@ import { PrismaService } from 'src/prisma/prisma.service';
 import { AutenticacaoDTO } from './dto';
 import * as argon from 'argon2';
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AutenticacaoService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private jwt: JwtService,
+    private config: ConfigService,
+  ) {}
+
+  async inscrever(dto: AutenticacaoDTO) {
+    // gerar senha hash
+
+    const hash = await argon.hash(dto.senha);
+    // salvar usuario no banco de dados
+
+    try {
+      const usuario = await this.prisma.usuario.create({
+        data: {
+          email: dto.email,
+          senhaHash: hash,
+        },
+      });
+
+      return this.criarToken(usuario.id, usuario.email);
+    } catch (erro) {
+      if (erro instanceof PrismaClientKnownRequestError) {
+        if (erro.code === 'P2002') {
+          throw new ForbiddenException('Credenciais já são usadas.');
+        }
+      }
+
+      throw erro;
+    }
+  }
+
   async entrar(dto: AutenticacaoDTO) {
     // encontrar usuario pelo email
     const usuario = await this.prisma.usuario.findUnique({
@@ -26,37 +59,28 @@ export class AutenticacaoService {
       throw new ForbiddenException('Credenciais incorretas.');
     }
 
-    delete usuario.senhaHash;
     // retornar o usuario
-    return usuario;
+    return this.criarToken(usuario.id, usuario.email);
   }
 
-  async inscrever(dto: AutenticacaoDTO) {
-    // gerar senha hash
+  async criarToken(
+    usuarioId: number,
+    email: string,
+  ): Promise<{ token_acesso: string }> {
+    const dados = {
+      sub: usuarioId,
+      email,
+    };
 
-    const hash = await argon.hash(dto.senha);
-    // salvar usuario no banco de dados
+    const segredo = this.config.get('JWT_SECRET');
 
-    try {
-      const usuario = await this.prisma.usuario.create({
-        data: {
-          email: dto.email,
-          senhaHash: hash,
-        },
-      });
+    const token = await this.jwt.signAsync(dados, {
+      expiresIn: '15m',
+      secret: segredo,
+    });
 
-      delete usuario.senhaHash;
-
-      // retornar usuario salvo
-      return usuario;
-    } catch (erro) {
-      if (erro instanceof PrismaClientKnownRequestError) {
-        if (erro.code === 'P2002') {
-          throw new ForbiddenException('Credenciais já são usadas.');
-        }
-      }
-
-      throw erro;
-    }
+    return {
+      token_acesso: token,
+    };
   }
 }
