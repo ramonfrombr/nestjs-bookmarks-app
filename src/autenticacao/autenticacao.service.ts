8986f76bfe16656264debefb9b98a945181ce5df
ramonfrombr <ramonfrombr>
2023-10-28 23:41:54 -0300

    06-funcionalidade-entrar

--- src/autenticacao/autenticacao.service.ts
+++ src/autenticacao/autenticacao.service.ts
@@ -7,8 +7,28 @@ import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 @Injectable()
 export class AutenticacaoService {
   constructor(private prisma: PrismaService) {}
-  entrar() {
-    return { msg: 'Entrou' };
+  async entrar(dto: AutenticacaoDTO) {
+    // encontrar usuario pelo email
+    const usuario = await this.prisma.usuario.findUnique({
+      where: {
+        email: dto.email,
+      },
+    });
+    // se usuario nao existir, mostrar erro
+    if (!usuario) {
+      throw new ForbiddenException('Credenciais incorretas.');
+    }
+
+    // comparar senhas
+    const senhasIguais = await argon.verify(usuario.senhaHash, dto.senha);
+    // se senha não for correta, mostrar erro
+    if (!senhasIguais) {
+      throw new ForbiddenException('Credenciais incorretas.');
+    }
+
+    delete usuario.senhaHash;
+    // retornar o usuario
+    return usuario;
   }
 
   async inscrever(dto: AutenticacaoDTO) {
